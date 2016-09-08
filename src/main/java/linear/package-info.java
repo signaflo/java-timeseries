e62de9f43b8670f19f2b9cd750ac2976d814150e
Jacob Rachiele <jrachiele@bybaxter.com>
2016-09-08 01:49:32 -0500

    Add marker packages for future development.

---
+++ src/main/java/linear/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2016 Jacob Rachiele
+ */
+
+/** Marker package for future development work. This package will contain linear algebra classes.
+ * @author Jacob Rachiele
+ *
+ */
+package linear;
---
+++ src/main/java/optim/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2016 Jacob Rachiele
+ */
+
+/**
+ * Marked for future development work. This package will contain optimization methods as the name suggests.
+ * @author Jacob Rachiele
+ *
+ */
+package optim;
