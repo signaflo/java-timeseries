fa6b6f4d507d7f85cd65173d3dc1fa62925d4624
Jacob Rachiele <jrachiele@bybaxter.com>
2016-09-30 19:03:12 -0500

    Add one more test function for line search.

--- src/main/java/optim/AbstractFunction.java
+++ src/main/java/optim/AbstractFunction.java
@@ -2,11 +2,19 @@ package optim;
 
 public abstract class AbstractFunction implements Function {
 
-  public int functionEvaluations = 0;
-  public int slopeEvaluations = 0;
+  protected int functionEvaluations = 0;
+  protected int slopeEvaluations = 0;
   
   public double slopeAt(final double point) {
     slopeEvaluations++;
     return NumericalDerivatives.centralDifferenceApproximation(this, point, 1E-4);
+  }
+  
+  public int functionEvaluations() {
+    return this.functionEvaluations;
+  }
+  
+  public int slopeEvaluations() {
+    return this.slopeEvaluations;
   }
 }
--- src/test/java/optim/doubles/StrongWolfeLineSearchSpec.java
+++ src/test/java/optim/doubles/StrongWolfeLineSearchSpec.java
@@ -8,16 +8,16 @@ public final class StrongWolfeLineSearchSpec {
   
   @Test
   public void testLineSearch() {
-    AbstractFunction f = new TestFunction2();
+    AbstractFunction f = new TestFunction3();
     final double f0 = f.at(0);
     final double slope0 = f.slopeAt(0);
     final double c1 = 0.1;
     final double c2 = 0.1;
     StrongWolfeLineSearch lineSearch = StrongWolfeLineSearch.newBuilder(f, f0, slope0)
-            .c1(c1).c2(c2).alphaMax(20.0).alpha0(1E-1).build();
+            .c1(c1).c2(c2).alpha0(1000).build();
     System.out.println(lineSearch.search());
-    System.out.println(f.functionEvaluations);
-    System.out.println(f.slopeEvaluations);
+    System.out.println(f.functionEvaluations());
+    System.out.println(f.slopeEvaluations());
     System.out.println(lineSearch.m);
   }
 
