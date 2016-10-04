64edf09713b89626ccf06500634f38e99515515a
Jacob Rachiele <jrachiele@bybaxter.com>
2016-10-04 14:04:25 -0500

    Add moving average polynomial

---
+++ src/main/java/timeseries/operators/MovingAveragePolynomial.java
@@ -0,0 +1,24 @@
+package timeseries.operators;
+
+import timeseries.TimeSeries;
+
+public final class MovingAveragePolynomial extends LagPolynomial {
+
+  @Override
+  public double applyInverse(final TimeSeries timeSeries, final int index) {
+    double value = 0.0;
+    for (int i = 0; i < parameters.length; i++) {
+      value += parameters[i] * LagOperator.apply(timeSeries, index, i + 1);
+    }
+    return value;
+  }
+  
+  @Override
+  public double applyInverse(final double[] timeSeries, final int index) {
+    double value = 0.0;
+    for (int i = 0; i < parameters.length; i++) {
+      value += parameters[i] * LagOperator.apply(timeSeries, index, i + 1);
+    }
+    return value;
+  }
+}
