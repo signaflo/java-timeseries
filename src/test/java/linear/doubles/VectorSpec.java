0b5a7e750400daf7d83c3b53cde69395dabf6f0b
Jacob Rachiele <jrachiele@bybaxter.com>
2016-10-27 01:16:37 -0500

    Add tests for linear.doubles Vector

--- src/test/java/linear/doubles/VectorSpec.java
+++ src/test/java/linear/doubles/VectorSpec.java
@@ -4,7 +4,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public final class VectorSpec {
 
@@ -12,11 +14,41 @@ public final class VectorSpec {
   private Vector vec2;
   private Vector vec3;
 
+  @Rule
+  public ExpectedException exception = ExpectedException.none();
+
   @Before
   public void beforeMethod() {
     vec1 = new Vector(3.0, 4.0, 7.5);
     vec2 = new Vector(4.5, 5.0, 2.0, 5.5);
     vec3 = new Vector(6.0, 2.5, 10.0);
+  }
+
+  @Test
+  public void whenAxpyThenResultCorrect() {
+    Vector result = vec1.axpy(vec3, 5.0);
+    Vector expected = new Vector(21.0, 22.5, 47.5);
+    assertThat(result, is(equalTo(expected)));
+  }
+
+  @Test
+  public void whenSumThenResultCorrect() {
+    double result = vec1.sum();
+    assertThat(result, is(equalTo(14.5)));
+    result = vec1.sumOfSquares();
+    assertThat(result, is(equalTo(81.25)));
+  }
+
+  @Test
+  public void whenEqualsAndHashCodeThenValuesCorrect() {
+    assertThat(vec1.equals(vec2), is(false));
+    assertThat(vec1.hashCode(), is(not(vec2.hashCode())));
+    Vector newVec = new Vector(3.0, 4.0, 7.5);
+    assertThat(vec1.equals(newVec), is(true));
+    assertThat(vec1.hashCode(), is(newVec.hashCode()));
+    assertThat(vec1.equals(new Object()), is(false));
+    assertThat(vec1.equals(null), is(false));
+    assertThat(vec1.equals(vec1), is(true));
   }
   
   @Test
@@ -26,6 +58,14 @@ public final class VectorSpec {
         8.0, 22.0, 33.75, 37.5, 15.0, 41.25};
     assertThat(matrix.data(), is(equalTo(expected)));
   }
+
+  @Test
+  public void whenDotProductZeroLengthVectorExceptionThrown() {
+    Vector empty = new Vector();
+    exception.expect(IllegalArgumentException.class);
+    vec1.dotProduct(empty);
+  }
+
 
   @Test
   public void whenVectorsAddedThenSumCorrect() {
