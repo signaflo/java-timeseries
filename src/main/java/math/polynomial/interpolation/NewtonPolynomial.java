cd520da72eeb69e61c64a47ee8fd86599fed6ab6
Jacob Rachiele <jrachiele@bybaxter.com>
2016-11-01 21:17:33 -0500

    update divided differences

--- src/main/java/math/polynomial/interpolation/NewtonPolynomial.java
+++ src/main/java/math/polynomial/interpolation/NewtonPolynomial.java
@@ -5,16 +5,18 @@
 
 package math.polynomial.interpolation;
 
+import math.function.QuadraticFunction;
+
 /**
  * @author Jacob Rachiele
  */
-public final class DividedDifference {
+final class NewtonPolynomial {
 
   private final double[] point;
   private final double[] value;
   private final double[] coefficients;
 
-  public DividedDifference(final double[] point, final double[] value) {
+  NewtonPolynomial(final double[] point, final double[] value) {
     if (point.length != value.length) {
       throw new IllegalArgumentException("There must be one function value for each point, "
           + "but there were " + point.length + " points and " + value.length + " values.");
@@ -26,31 +28,45 @@ public final class DividedDifference {
     this.point = point.clone();
     this.value = value.clone();
     this.coefficients = new double[value.length];
-    this.coefficients[0] = value[0];
-    if (point.length > 1) {
-      this.coefficients[1] = (value[1] - value[0]) / (point[1] - point[0]);
-    }
-    for (int i = 2; i < point.length; i++) {
+    for (int i = 0; i < point.length; i++) {
       this.coefficients[i] = getDividedDifference(0, i);
     }
   }
 
-  public final double[] coefficients() {
+  final double[] coefficients() {
     return this.coefficients.clone();
   }
 
-  public final double getCoefficient(final int i) {
+  final double getCoefficient(final int i) {
     return this.coefficients[i];
   }
 
-  public final double getDividedDifference(final int start, final int end) {
+  private double getDividedDifference(final int start, final int end) {
     int k = end - start;
     if (k == 0) {
       return value[end];
     } else if (k == 1) {
       return (value[end] - value[start]) / (point[end] - point[start]);
     } else {
-      return (getDividedDifference(start + 1, end) - getDividedDifference(start, end - 1)) / (point[end] - point[0]);
+      return (getDividedDifference(start + 1, end) - getDividedDifference(start, end - 1)) /
+          (point[end] - point[0]);
     }
+  }
+
+  public final double evaluateAt(double x) {
+    double product = 1.0;
+    double result = coefficients[0];
+    for (int i = 1; i < coefficients.length; i++) {
+      product *= x - point[i - 1];
+      result += coefficients[i] * product;
+    }
+    return result;
+  }
+
+  public final QuadraticFunction simplify() {
+    double a = coefficients[2];
+    double b = coefficients[1] - coefficients[2] * (point[0] + point[1]);
+    double c = coefficients[0] - coefficients[1] * point[0] + coefficients[2] * point[0] * point[1];
+    return new QuadraticFunction(a, b, c);
   }
 }
