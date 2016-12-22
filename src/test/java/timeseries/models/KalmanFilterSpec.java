a92e27d2fc57a159558efc5065b85e9ca578fb23
Jacob Rachiele <jrachiele@bybaxter.com>
2016-12-21 21:29:53 -0600

    Add comment for R validation to starma tests

--- src/test/java/timeseries/models/KalmanFilterSpec.java
+++ src/test/java/timeseries/models/KalmanFilterSpec.java
@@ -29,12 +29,17 @@ public class KalmanFilterSpec {
     System.out.println("Time taken: " + (end - start) + " millis.");
   }
 
+  /*
+  printArima <- function(phi = numeric(), theta = numeric()) {
+    model <- makeARIMA(phi = phi, theta = theta, Delta = numeric())
+    cat(paste(round(model$Pn[lower.tri(model$Pn, diag = TRUE)], 6), collapse = ", "))
+  }
+  */
   @Test
   public void whenManyMoreARThanMACoeffsThenStarmaOutputCorrect() {
     double[] phi = {0.5, 0.2, -0.3, 0.1};
     double[] theta = {0.7};
     double[] P = ArmaKalmanFilter.getInitialStateCovariance(phi, theta);
-    // makeARIMA(phi = c(0.5, 0.2, -0.3, 0.1), theta = c(0.7), Delta = numeric())$Pn
     double[] expected = {3.201238, 0.854377, -0.610805, 0.2455, 0.546552, -0.03122, 0.002944,
         0.172824, -0.071487, 0.032012};
     assertArrayEquals(expected, P, 1E-6);
