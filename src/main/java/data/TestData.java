ced827da2abdae688ebb096a207e2539195f1d6a
Jacob Rachiele <jrachiele@bybaxter.com>
2017-04-13 17:12:24 -0500

    Update docs

--- src/main/java/data/TestData.java
+++ src/main/java/data/TestData.java
@@ -22,7 +22,7 @@ public final class TestData {
 
     /**
      * <a target="_blank" href="https://datamarket.com/data/set/22xx/monthly-sales-of-tasty-cola#!ds=22xx&display=line"
-     * >monthly sales of tasty cola</a> (Bowerman and O’Connell (1993)), made available by Rob J Hyndman at DataMarket.
+     * >monthly sales of tasty cola</a> (Bowerman and O'Connell (1993)), made available by Rob J Hyndman at DataMarket.
      *
      * @return the monthly sales of tasty cola data set.
      */
