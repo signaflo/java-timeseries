c38a09f540c5ffbf4a02e5c332f6cdf0ad7dba05
Jacob Rachiele <jrachiele@bybaxter.com>
2017-05-03 19:02:17 -0500

    Add random variable class

---
+++ src/main/java/math/probability/RandomVariable.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2017 Jacob Rachiele
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to
+ * do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *
+ * Jacob Rachiele
+ */
+
+package math.probability;
+
+import lombok.NonNull;
+import math.Real;
+
+import java.util.function.Function;
+
+public final class RandomVariable<T> implements Function<T, Real> {
+
+    private final Function<T, Real> function;
+
+    RandomVariable(Function<T, Real> function) {
+        this.function = function;
+    }
+
+    @Override
+    public Real apply(@NonNull T t) {
+        return this.function.apply(t);
+    }
+}
